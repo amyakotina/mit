b84b9587e4ae4a840064a2d37ce8f8b224a94ade
amyakotina <a-myakotina@mail.ru>
2024-05-05 16:58:23 +0400

    we added the function fib(n), which recursively calculates the n number of Fibbonichi, the sequence starts with (1, 1, ...)

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+int fib(int n) {
+    if(n <= 1) {
+        return n;
+    }
+    return fib (n-1) + fib (n-2);
+}
+
 int main () {
     std::cout<< "Hello World" << std::endl;
+    int n;
+    std:: cout << "n = ";
+    std:: cin >> n;
+    std:: cout << "Число Фибоначчи для n = " << n << ": "<< fib(n);
     return 0;
 }
