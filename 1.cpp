e8c04830d314e0bb9c4eb486e4175eb02059e179
amyakotina <a-myakotina@mail.ru>
2024-05-05 17:23:33 +0400

    we changed the fib(n) function so that the sequence of numbers starts with the values (0, 1, ...)

--- 1.cpp
+++ 1.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 
 int fib(int n) {
-    if(n <= 1) {
-        return n;
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
     }
-    return fib (n-1) + fib (n-2);
+
+    int prev = 0;
+    int current = 1;
+    int result;
+
+    for (int i = 2; i <= n; ++i) {
+        result = prev + current;
+        prev = current;
+        current = result;
+    }
+
+    return prev; 
 }
+
 
 int main () {
     std::cout<< "Hello World" << std::endl;
